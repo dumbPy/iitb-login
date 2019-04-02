1fc7e7b44b5f32713ad92bfaa54d42d1ed45e3b4
Sufiyan <dumbpyx@gmail.com>
2019-04-02 20:16:05 +0530

    setup.py added and dist built

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(  name='iitb_login',
+        version='0.1.0',
+        description='iitb login script with ssh key based credentials encryption',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url='https://github/com/dumbPy/iitb_login',
+        author='Sufiyan Adhikari',
+        author_email='dumbpyx@gmail.com',
+        python_requires='>=3.3',
+        install_requires=['paramiko', 'scrapy', 'click',
+                            'argparse','cryptography','getpass','pickle',
+                        ],
+        scripts=['scripts/iitb'],
+        # There are no packages, just a login script
+        packages=[]
+        )

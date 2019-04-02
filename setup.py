716bfbb76166653eec9fae1ebfe7125d19d6fa81
Sufiyan Adhikari <dumbpyx@gmail.com>
2019-04-02 20:38:23 +0530

    ver 1.0.1 changing name or package

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(  name='iitb-login',
-        version='0.1.0',
+        version='0.1.1',
         description='iitb login script with ssh key based credentials encryption',
         long_description=long_description,
         long_description_content_type='text/markdown',
